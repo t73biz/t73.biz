25979b317b429a0244612471676a94185188e6f6
Ronald Chaplin <rchaplin@t73.biz>
2015-10-21 13:50:46 -0500

    Implementing layout

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,20 @@
-console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
+var mainApp = angular.module('mainApp', ['ui.router']);
+
+mainApp.config(function($stateProvider, $urlRouterProvider) {
+	$urlRouterProvider.otherwise("/home");
+
+	// Now set up the states
+  $stateProvider
+	  .state('home', {
+		url: "/home",
+		templateUrl: "pages/home.html"
+	  })
+	  .state('about', {
+		url: "/about",
+		templateUrl: "pages/about.html"
+	  })
+	  .state('contact', {
+        url: "/contact",
+        templateUrl: "pages/contact.html"
+      })
+});
