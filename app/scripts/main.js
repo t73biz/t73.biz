057e06fa3dc2fbe321da6cb4f32d1ce2d2e04be3
Ronald Chaplin <rchaplin@t73.biz>
2015-10-21 16:01:41 -0500

    Implementing Articles

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,17 +4,45 @@ mainApp.config(function($stateProvider, $urlRouterProvider) {
 	$urlRouterProvider.otherwise("/home");
 
 	// Now set up the states
-  $stateProvider
-	  .state('home', {
+$stateProvider
+	.state('home', {
 		url: "/home",
-		templateUrl: "pages/home.html"
-	  })
-	  .state('about', {
+		templateUrl: "pages/home.html",
+		controller: function($scope) {
+			$scope.articles = [
+				{
+					href: "bootstrap-alert-plugin",
+					title: "Bootstrap Alert Plugin"
+				},
+				{
+					href: "sitepoint-versioning-newsletter",
+					title: "Sitepoint Versioning Newsletter"
+				},
+				{
+					href: "translocation-bundle",
+					title: "Translocation Bundle"
+				}
+			];
+		}
+	})
+	.state('about', {
 		url: "/about",
 		templateUrl: "pages/about.html"
-	  })
-	  .state('contact', {
-        url: "/contact",
-        templateUrl: "pages/contact.html"
-      })
+	})
+	.state('contact', {
+		url: "/contact",
+		templateUrl: "pages/contact.html"
+	})
+	.state('bootstrap-alert-plugin', {
+		url: "/bootstrap-alert-plugin",
+		templateUrl: "pages/bootstrap-alert-plugin.html"
+	})
+	.state('sitepoint-versioning-newsletter', {
+		url: "/sitepoint-versioning-newsletter",
+		templateUrl: "pages/sitepoint-versioning-newsletter.html"
+	})
+	.state('translocation-bundle', {
+		url: "/translocation-bundle",
+		templateUrl: "pages/translocation-bundle.html"
+	})
 });
