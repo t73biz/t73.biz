8d186786d7ab87c11ed412573ea443b39008b6bd
Ronald Chaplin <rchaplin@t73.biz>
2015-10-24 11:53:06 -0500

    Writing new article Cakephp 3 Maintenance Mode Tutorial Moving the articles to a new section within pages, for better structuring. Adjusted the width for larger displays with a min-width of 1024px

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,26 +3,32 @@ var mainApp = angular.module('mainApp', ['ui.router']);
 mainApp.config(function($stateProvider, $urlRouterProvider) {
 	$urlRouterProvider.otherwise("/home");
 
+var articleList = [
+	{
+		href: "bootstrap-alert-plugin",
+		title: "Bootstrap Alert Plugin"
+	},
+	{
+		href: "cakephp-3-maintenance-mode",
+		title: "Cakephp 3 Maintenance Mode Tutorial"
+	},
+	{
+		href: "sitepoint-versioning-newsletter",
+		title: "Sitepoint Versioning Newsletter"
+	},
+	{
+		href: "translocation-bundle",
+		title: "Translocation Bundle"
+	}
+];
+
 	// Now set up the states
 $stateProvider
 	.state('home', {
 		url: "/home",
 		templateUrl: "pages/home.html",
 		controller: function($scope) {
-			$scope.articles = [
-				{
-					href: "bootstrap-alert-plugin",
-					title: "Bootstrap Alert Plugin"
-				},
-				{
-					href: "sitepoint-versioning-newsletter",
-					title: "Sitepoint Versioning Newsletter"
-				},
-				{
-					href: "translocation-bundle",
-					title: "Translocation Bundle"
-				}
-			];
+			$scope.articles = articleList;
 		}
 	})
 	.state('about', {
@@ -35,14 +41,18 @@ $stateProvider
 	})
 	.state('bootstrap-alert-plugin', {
 		url: "/bootstrap-alert-plugin",
-		templateUrl: "pages/bootstrap-alert-plugin.html"
+		templateUrl: "pages/articles/bootstrap-alert-plugin.html"
+	})
+	.state('cakephp-3-maintenance-mode', {
+		url: "/cakephp-3-maintenance-mode",
+		templateUrl: "pages/articles/cakephp-3-maintenance-mode.html"
 	})
 	.state('sitepoint-versioning-newsletter', {
 		url: "/sitepoint-versioning-newsletter",
-		templateUrl: "pages/sitepoint-versioning-newsletter.html"
+		templateUrl: "pages/articles/sitepoint-versioning-newsletter.html"
 	})
 	.state('translocation-bundle', {
 		url: "/translocation-bundle",
-		templateUrl: "pages/translocation-bundle.html"
+		templateUrl: "pages/articles/translocation-bundle.html"
 	})
 });
